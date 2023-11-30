d242c2e0b9e329635748e8520ca106fd3c24090c
AzureAD\EugeneFurer <eugene.furer@thirdwaverx.com>
2023-11-30 16:50:16 -0500

    added web poject

--- pricebuilder.net.db/WorkItem.cs
+++ pricebuilder.net.db/WorkItem.cs
@@ -7,7 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace pricebuilder.net.db
+namespace ef.pricebuilder.net.db
 {
     using System;
     using System.Collections.Generic;
---
+++ pricebuilder.net.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pricebuilder.net.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
